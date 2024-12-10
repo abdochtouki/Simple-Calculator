f4ee9af28cd8a8855d7f8efb6b715e82763e1768
abdoChtoukiDev <abdochtouki88@gmail.com>
2024-12-10 16:18:11 +0100

    SIM-9 4

--- MyCode.py
+++ MyCode.py
@@ -1,3 +1,5 @@
+# calculator.py
+
 def add_numbers(a, b):
     return a + b
 
@@ -5,17 +7,14 @@ def sub_numbers(a, b):
     return a - b
 
 def get_user_input():
-    # Vérifiez si les entrées sont déjà définies par des variables d'environnement, sinon demandez à l'utilisateur
-    import os
-    a = float(os.getenv("NUM1", 1))  # Valeur par défaut de 1 si NUM1 n'est pas définie
-    b = float(os.getenv("NUM2", 2))  # Valeur par défaut de 2 si NUM2 n'est pas définie
+    a = float(input("first number:"))
+    b = float(input("second number:"))
     return a, b
 
 if __name__ == "__main__":
     num1, num2 = get_user_input()
     add_result = add_numbers(num1, num2)
     sub_result = sub_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is: {add_result}")
-    print(f"The difference between {num1} and {num2} is: {sub_result}")
-    
+    print("The sum of {} and {} is: {}".format(num1, num2, add_result))
+    print("The sum of {} and {} is: {}".format(num1, num2, sub_result))
     print("END CI/CD :)")
